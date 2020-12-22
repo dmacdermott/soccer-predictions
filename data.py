713cc151360263ba45525d85d75c3805531cec4c
dmacdermott <damianmacdermott@gmail.com>
2020-12-22 18:21:58 +0900

    added func to get weekly matches

--- data.py
+++ data.py
@@ -8,3 +8,14 @@ def getNewData():
         "https://api.footystats.org/league-matches?key=test85g57&league_id=4759").json()
     with open("data/all_matches.json", "w", encoding='utf-8') as outfile:
         json.dump(allMatches, outfile,  ensure_ascii=False, indent=4)
+
+
+def getWeeksMatches():
+    with open("data/all_matches.json") as matches:
+        matches = json.load(matches)
+        for match in matches["data"]:
+            if match["game_week"] == 14:
+                print(match["home_name"] + " vs " + match["away_name"])
+
+
+getWeeksMatches()
