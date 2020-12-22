6586bac9adc34fd4f04ca652dc3a47bb6b29ae1d
dmacdermott <damianmacdermott@gmail.com>
2020-12-22 18:40:57 +0900

    added gitignore models and team data

--- data.py
+++ data.py
@@ -2,6 +2,12 @@
 import requests
 import json
 
+# DATA NEEDED
+# home_name, away_name, game_week, corners_fh_home,
+# corners_fh_away, shotsAVG_home, shotsAVG_away,
+# shotsOnTargetAVG_home, shotsOnTargetAVG_away
+# "B365H", "B365D", "B365A"
+
 
 def getNewData():
     allMatches = requests.get(
@@ -10,12 +16,18 @@ def getNewData():
         json.dump(allMatches, outfile,  ensure_ascii=False, indent=4)
 
 
-def getWeeksMatches():
+# Link with date for TeamData to check past results
+# "https: // api.footystats.org/league-teams?key=example & season_id=4759 & include=stats & max_time=1600732800"
+def getTeamData():
+    teamData = requests.get(
+        "https://api.footystats.org/league-teams?key=example&season_id=4759&include=stats").json()
+    with open("data/team_data.json", "w", encoding='utf-8') as outfile:
+        json.dump(teamData, outfile,  ensure_ascii=False, indent=4)
+
+
+def getWeeksMatches(wkNumber):
     with open("data/all_matches.json") as matches:
         matches = json.load(matches)
         for match in matches["data"]:
-            if match["game_week"] == 14:
+            if match["game_week"] == wkNumber:
                 print(match["home_name"] + " vs " + match["away_name"])
-
-
-getWeeksMatches()
