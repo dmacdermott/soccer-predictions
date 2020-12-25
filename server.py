ed20a5765423eb5c5239da9cbc7834b10829ee55
dmacdermott <damianmacdermott@gmail.com>
2020-12-25 11:42:06 +0900

    added procfile and requirements txt

--- server.py
+++ server.py
@@ -4,7 +4,14 @@ import app as func
 import requests
 import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route("/matches/<week>")
